42f73c6a07342954e961039c80cb9bda7de8eee8
Christian Luna <Christian Luna@DESKTOP-PUFBE03>
2021-11-03 16:16:36 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppArreglos/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsAppArreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ubicaciónTextBox = new System.Windows.Forms.TextBox();
+            this.díaTextBox = new System.Windows.Forms.TextBox();
+            this.valorTextBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cambiar el valor en la ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ubicación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "del día";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(153, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "El total de precipitación pluvial es";
+            // 
+            // ubicaciónTextBox
+            // 
+            this.ubicaciónTextBox.Location = new System.Drawing.Point(187, 45);
+            this.ubicaciónTextBox.Name = "ubicaciónTextBox";
+            this.ubicaciónTextBox.Size = new System.Drawing.Size(66, 20);
+            this.ubicaciónTextBox.TabIndex = 5;
+            this.ubicaciónTextBox.Text = "0";
+            // 
+            // díaTextBox
+            // 
+            this.díaTextBox.Location = new System.Drawing.Point(306, 45);
+            this.díaTextBox.Name = "díaTextBox";
+            this.díaTextBox.Size = new System.Drawing.Size(61, 20);
+            this.díaTextBox.TabIndex = 6;
+            this.díaTextBox.Text = "0";
+            // 
+            // valorTextBox
+            // 
+            this.valorTextBox.Location = new System.Drawing.Point(392, 45);
+            this.valorTextBox.Name = "valorTextBox";
+            this.valorTextBox.Size = new System.Drawing.Size(66, 20);
+            this.valorTextBox.TabIndex = 7;
+            this.valorTextBox.Text = "0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 86);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(424, 74);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 232);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.valorTextBox);
+            this.Controls.Add(this.díaTextBox);
+            this.Controls.Add(this.ubicaciónTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Precipitación pluvial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ubicaciónTextBox;
+        private System.Windows.Forms.TextBox díaTextBox;
+        private System.Windows.Forms.TextBox valorTextBox;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WindowsFormsAppArreglos/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppArreglos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            int[,] datosLluvia =
+            {{0, 7, 3, 28, 5, 6, 3},
+            {12, 3, 5, 7, 12, 5, 8},
+            { 8, 5, 2, 1, 1, 4, 7}};
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                textBox4.Clear();
+                for (int ubicación = 0; ubicación <= 2; ubicación++)
+                {
+                    for (int númeroDía = 0; númeroDía <= 6; númeroDía++)
+                    {
+                        textBox4.AppendText(Convert.ToString
+
+                        (datosLluvia[ubicación, númeroDía]) + "\t");
+
+                    }
+                    textBox4.AppendText("\r\n");
+                }
+        
+            }
+
+           
+            {
+                int valorDatos;
+                int númeroDía;
+                int ubicación;
+                númeroDía = Convert.ToInt32(díaTextBox.Text);
+                ubicación = Convert.ToInt32(ubicaciónTextBox.Text);
+                valorDatos = Convert.ToInt32(valorTextBox.Text);
+                datosLluvia[ubicación, númeroDía] = valorDatos;
+                
+            }
+
+            {
+                int total = 0;
+                for (int ubicación = 0; ubicación <= 2; ubicación++)
+                {
+                    for (int númeroDía = 0; númeroDía <= 6; númeroDía++)
+                    {
+                        total = total + datosLluvia[ubicación, númeroDía];
+                    }
+                }
+                label4.Text = "el total de precipitación pluvial es " + Convert.ToString(total);
+            }
+
+
+
+        }
+    }
+}
